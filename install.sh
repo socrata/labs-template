db6768c9784e1698f084ad84e9d43887fdb93cd5
Chris Metcalf <chris@chrismetcalf.net>
2014-09-10 16:44:35 -0700

    Tweak to the install script

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ echo "Copying in files we don't want to hyperlink..."
 cp -n _template/_config.yml _template/search.json _template/related.json .
 
 echo "Linking in template files..."
-./_template/dircombine '.*' '(dircombine|install.sh|.git)' . _template
+./_template/dircombine '.*' '(dircombine|install.sh)' . _template
 
 echo "Done!"
